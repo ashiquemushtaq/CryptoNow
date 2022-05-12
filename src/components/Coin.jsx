6e13e771294747dfdcd1c6cc36326bc7eb85635a
Ashique Mushtaq <ashiquemushtaqt@gmail.com>
2022-05-12 11:34:27 +0530

    Style Update

--- src/components/Coin.jsx
+++ src/components/Coin.jsx
@@ -21,36 +21,38 @@ const Coin = () => {
         <button className="btn-back">Back</button>
       </Link>
       {coin ? (
-        <div className="coin-details">
-          <img src={coin.coin.icon} alt="coin-icon" />
-          <div className="content">
-            <h2>
-              <span>{coin.coin.name}</span>
-            </h2>
-            <h3>
-              Rank:<span>{coin.coin.rank}</span>
-            </h3>
-            <h4>
-              Symbol:<span>{coin.coin.symbol}</span>
-            </h4>
-            <h4>
-              Price:<span>{coin.coin.price} INR</span>
-            </h4>
-            <h4>
-              Volume:<span>{coin.coin.volume}</span>
-            </h4>
-            <h4>
-              Volume:<span>{coin.coin.volume}</span>
-            </h4>
-            <h4>
-              Circulating Supply:<span>{coin.coin.availableSupply}</span>
-            </h4>
-            <a href={coin.coin.websiteUrl} target="_blank" rel="noreferrer">
-              Website
-            </a>
-            <a href={coin.coin.twitterUrl} target="_blank" rel="noreferrer">
-              Twitter
-            </a>
+        <div className="coin">
+          <div className="coin-details">
+            <img src={coin.coin.icon} alt="coin-icon" />
+            <div className="content">
+              <h2>
+                <span>{coin.coin.name}</span>
+              </h2>
+              <h3>
+                Rank:<span>{coin.coin.rank}</span>
+              </h3>
+              <h4>
+                Symbol:<span>{coin.coin.symbol}</span>
+              </h4>
+              <h4>
+                Price:<span>{coin.coin.price} INR</span>
+              </h4>
+              <h4>
+                Volume:<span>{coin.coin.volume}</span>
+              </h4>
+              <h4>
+                Volume:<span>{coin.coin.volume}</span>
+              </h4>
+              <h4>
+                Circulating Supply:<span>{coin.coin.availableSupply}</span>
+              </h4>
+              <a href={coin.coin.websiteUrl} target="_blank" rel="noreferrer">
+                Website
+              </a>
+              <a href={coin.coin.twitterUrl} target="_blank" rel="noreferrer">
+                Twitter
+              </a>
+            </div>
           </div>
         </div>
       ) : (
